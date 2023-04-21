09e333d4695de1e1b05c91611153bad08fb48142
jck-bit <jckkinyanjui@gmail.com>
2023-04-21 16:19:31 +0300

    added the sales table

--- src/widgets/Sales.tsx
+++ src/widgets/Sales.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import '../SalesTable.css';
+import { setSales } from '../state';
+import { useDispatch, useSelector } from 'react-redux';
+import { Sale } from '../types';
 
-const Sales = () => {
+function SalesTable() {
+  const dispatch = useDispatch();
+  const sales = useSelector((state: any) => state.sales);
+
+  const getSales = async () => {
+    const response = await fetch( "https://stockify-store-management.vercel.app/sales", {
+      method: "GET",
+    });
+    const data = await response.json();
+    console.log(data);
+    dispatch(setSales({sales: data.sales}));
+  };
+  
+  useEffect(() =>{
+    getSales()
+  },[])
   return (
-    <div>Sales</div>
-  )
+    <div>
+      <h1>Sales Report</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>product</th>
+            <th>Date Sold</th>
+            <th>Total Sale</th>
+            <th>User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sales && sales.map((sale: Sale) => {
+            return(
+            <tr key={sale.id}>
+              <td>{sale.product}</td>
+              <td>{sale.date}</td>
+              <td>{sale.total_sale}</td>
+              <td>user {sale.user}</td>
+            </tr>
+         ) })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Sales
+export default SalesTable;
