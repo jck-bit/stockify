0fdfe1e2782726898971c584622c4e41f21bbb75
jck-bit <jckkinyanjui@gmail.com>
2023-06-06 22:46:44 +0300

    if product.quantity is null, then out of stock<p>

--- src/widgets/store.tsx
+++ src/widgets/store.tsx
@@ -25,20 +25,22 @@ const OnlineStore = () => {
     } catch (error) {
       console.error('Error fetching products:', error);
     }
-  };
+  };  
 
   return (
     <div className="container">
-      <h1>Online Store Stock Keeping</h1>
+      <h4>Stocks Keeping</h4>
       <div className="row">
         {products.map((product:Product) => (
           <div className="col-md-4" key={product.id}>
             <div className="card mb-4">
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
+                {/* if the quantity is null, the product.quantity will be 0 */}
+                <p className='card-text'>{product.quantity === null ? "Out of stock" : product.quantity + " in stock"}</p>
                 <p className="card-text">{product.description}</p>
                 <p className="card-text">Price: ${product.price}</p>
-                <button className="btn btn-primary">Edit</button>
+                <button className="btn btn-primary rounded-0">Edit</button>
               </div>
             </div>
           </div>
