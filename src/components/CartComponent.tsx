22b193f614f46a392559acf88bcb9cb76d799372
jck-bit <jckkinyanjui@gmail.com>
2023-06-23 10:36:42 +0300

    changes to the cart component

--- src/components/CartComponent.tsx
+++ src/components/CartComponent.tsx
@@ -7,48 +7,47 @@ import { useSnackbar } from "notistack";
 import CountCart from './CountCart'
 
 const CartComponent = () => {
-    const dispatch  = useDispatch()
-    const cartItems = useSelector<Rootstate, CartItem[]>(state => state.cart);
-    const { enqueueSnackbar } = useSnackbar();
+  const dispatch = useDispatch();
+  const cartItems = useSelector<Rootstate, CartItem[]>(state => state.cart);
+  const { enqueueSnackbar } = useSnackbar();
 
-    const handleRemoveFromCart = (product: Product) => {
-        dispatch(deleteOneProduct(product.id));
-        enqueueSnackbar(`Item removed from your Cart`, {
-          variant: "warning",
-          autoHideDuration: 1500,
-        });
-      };
+  const handleRemoveFromCart = (product: Product) => {
+    dispatch(deleteOneProduct(product.id));
+    enqueueSnackbar(`Item removed from your Cart`, {
+      variant: "warning",
+      autoHideDuration: 1500,
+    });
+  };
 
   return (
     <div className="cart-items">
-
-    {cartItems.map((item: CartItem) => (
-      <div style={{borderTop:"0.5px solid #D3D3D3",  width:"100%" ,paddingBottom:"5px", paddingTop:"5px"}}>
-      <div className="card-body d-flex  justify-content-between align-items-center">
-        <div className="mr-5">
-          <img src={item.product_pic} alt={item.name} className="card-img" style={{width: '100px', height: '100px', marginRight:"10px"}}/>
-        </div>
-        <div className="card-details" style={{flexGrow:1, marginBottom:"10px"}}>
-          <h5 className="card-title">{item.name}</h5>
-          <h6 className="card-subtitle mb-2">{item.description}</h6>
-          <div>
-           <div className="card-text mb-3"> KES {item.price}</div>
-           <CountCart product={item}/>
+      {cartItems.map((item: CartItem) => (
+        <div key={item.id} style={{ borderTop: "0.5px solid #D3D3D3", width: "100%", paddingBottom: "5px", paddingTop: "5px" }}>
+          <div className="card-body d-flex justify-content-between align-items-center">
+            <div className="mr-5">
+              <img src={item.product_pic} alt={item.name} className="card-img" style={{ width: '100px', height: '100px', marginRight: "10px" }} />
+            </div>
+            <div className="card-details" style={{ flexGrow: 1, marginBottom: "10px" }}>
+              <h5 className="card-title">{item.name}</h5>
+              <h6 className="card-subtitle mb-2">{item.description}</h6>
+              <div className="card-text"> KES {item.price}</div>
+              <div className="buttons-container d-flex justify-content-center align-items-center">
+                <CountCart product={item} />
+                <div>
+                  <BsFillTrash3Fill
+                    onClick={() => handleRemoveFromCart(item)}
+                    style={{ transition: "color 0.3s", width: "20px", height: "20px", marginLeft: "20px" }}
+                    size={20}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          
         </div>
-        <div className="trash-icon" style={{cursor:"pointer"}}>
-        <BsFillTrash3Fill
-          onClick={() => handleRemoveFromCart(item)}
-          style={{ transition: "color 0.3s" ,width:"20px", height:"20px", marginRight:"0", marginLeft:"20px"}}
-          size={20}
-       />
-      </div>
-      </div>
-    </div> 
-    ))}
-  </div>  
-  )
+      ))}
+    </div>
+  );
 }
+
 
 export default CartComponent
